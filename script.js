9ab647ffcd5a1ebb53686349570b2ea9c17abd7f
Vang Huynh <vanghm@imt-soft.com>
2023-07-05 16:34:10 +0700

    fix audio

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var audio = document.getElementById("audio");
+var audio = new Audio();
 audio.src = '/mp3/Cho-Trong-Ai.mp3';
 audio.loop = true;
 
-window.addEventListener("load", function() {
-    audio.play();
+window.addEventListener("load", function () {
+    function animate() {
+        audio.play();
+        requestAnimationFrame(animate);
+    }
+    animate(0);
 })
